1b51f3d34bcf1401be6f01d5e0dca7586965d470
Isaac Cohen <icohen@leapmotion.com>
2014-04-30 13:08:37 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,103 @@
+ShaderLoader
+=====
+
+A Utility to help you keep your shaders in seperate files. 
+Please open an issue if you see bugs / how to make things better!
+
+If you make something using this, hit me up at isaaclandoncohen@gmail.com , or via twitter @Cabbibo
+
+Also helps to keep long utility functions, such as simplex noise
+out of shader files.
+
+Basic Usage
+=====
+
+Include the neccesary scripts. Currently ShaderLoader uses jquery for AJAX calls. Sorry about that...
+
+```
+<script src="path/to/ShaderLoader.js"></script>
+<script src="path/to/jquery.min.js"></script>
+```
+
+Initialize the ShaderLoader:
+
+```
+var pathToShaders = 'path/to/shaders';
+var pathToChunks  = 'path/to/chunks';
+var shaders = new ShaderLoader( pathToShaders , pathToChunks );
+```
+
+Create your shaders!
+
+```
+var vertShader  = shaders.load( 'vert.js' , 'VERT'  , 'vertex'      );
+var fragShader  = shaders.load( 'frag.js' , 'FRAG'  , 'fragment'    );
+var simShader   = shaders.load( 'sim.js'  , 'SIM'   , 'simulation'  );
+```
+
+
+Once they are loaded they will become part of the `shaders` object,
+
+```
+var vert  = shaders.vertexShaders.VERT;
+var frag  = shaders.fragmentShaders.FRAG;
+var sim   = shaders.simulationShaders.SIM;
+```
+
+NOW GO MAKE SOME PRETTY SHIT!
+
+More Info
+=====
+
+Using Shader Chunks
+----
+
+When creating a shader, you can add function defines that come from different files. It is important to note that ShaderLoader will look for the shader in the 'pathToChunks' directory. 
+
+```
+// Inside frag.js
+varying vec3 vPos;
+
+// Will look for simplex.js in the 'pathToChunks' directory
+$simplex
+
+void main(){
+
+  float noise = simplex( vPos );
+  gl_FragColor = vec4( noise , 1.0 );
+
+}
+```
+
+
+
+Loading
+----
+
+ShaderLoader will call a `beginLoad` and `endLoad` at the beginning and ending 
+every shader load. Thus, if you've got a loading bar, you can rewrite these
+functions, to do what you need for your loading needs.
+
+Remember that you can also access the total number of shaders that the ShaderLoader 
+has loaded by accessing the `shadersLoaded` attribute.
+
+For example, if I want to wait until all the shaders are loaded to initialize my scene, I might do the following:
+
+```
+var shaders = new ShaderLoader( 'pathTo/Shaders' , 'pathTo/Chunks' );
+
+var shadersToLoad = 3;
+
+shaders.endLoad = function(){
+  if( this.shadersLoaded == shadersToLoad ){
+    init();
+  }
+}
+
+shaders.load( 'frag.js' , 'FRAGMORTION' , 'fragment'    );
+shaders.load( 'vert.js' , 'VERTICAL'    , 'vertex'      );
+shaders.load( 'sim.js'  , 'SIMULACRA'   , 'simulation'  );
+
+// All Our Fancy Scene Stuff
+function init(){}
+```
---
+++ ShaderLoader.js
@@ -0,0 +1,169 @@
+
+  // Shader Loader will Load any shader you want,
+  // And be able to add in large functions ( such as noise )
+  // with regex inside the shader
+  function ShaderLoader( pathToShaders , pathToChunks ){
+
+    this.fragmentShaders    = {};
+    this.vertexShaders      = {};
+    this.simulationShaders  = {};
+
+    this.pathToShaders    = pathToShaders || "/" ;
+    this.pathToChunks     = pathToChunks || pathToShaders;
+
+    this.shaderChunks = {};
+
+    this.shadersLoaded = 0;
+   // this.loadShaderChunks( pathToChunks );
+
+  }
+
+
+
+  /*
+   
+     Loads in a shader chunk when told to by
+     onShaderLoaded.
+
+     it is important to know that the title of the
+     chunk needs to be the same as the reference in the shader
+
+     AKA, if I use:
+
+     $simplexNoise
+
+     I will need to create a file in the pathToChunks directory
+     called
+
+     simplexNoise.js
+
+
+  */
+  ShaderLoader.prototype.loadShaderChunk = function( type ){
+
+    var path = this.pathToChunks + "/" + type + ".js";
+
+    var self = this;
+    $.ajax({
+      url:path,
+      dataType:'text',
+      context:{
+        title:type,
+        path: path
+      },
+      complete: function( r ){
+        self.onChunkLoaded( r.responseText , this.title );
+      },
+      error:function( r ){
+        console.log( 'ERROR: Unable to Load Shader' + this.path );
+        self.onChunkLoaded( " NO SHADER LOADED " , this.title );
+      }
+    });
+
+  }
+  
+  ShaderLoader.prototype.onChunkLoaded = function( chunk , title ){
+
+    this.shaderChunks[title] = chunk;
+    
+  }
+  
+  /*
+  
+     This function Loads a shader with whatever title/
+     type we prefer. 
+
+  */
+  ShaderLoader.prototype.load = function( shader , title , type ){
+ 
+    var self = this;
+
+    this._beginLoad(  shader , title , type );
+
+
+    // request the file over AJAX
+    $.ajax({
+      url: self.pathToShaders +"/" + shader + ".js" ,
+      dataType: 'text',
+      context: {
+        type: type 
+      },
+      complete: function(r){
+        self.onShaderLoaded( r.responseText , title , this.type );
+      }
+    });
+
+  }
+
+  /*
+   
+     Once a Shader is loaded, check to see if there are any extra chunks 
+     we need to find and pull in. 
+
+     Will recall itself, until the chunk has been loaded in
+
+  */
+  ShaderLoader.prototype.onShaderLoaded = function( shaderText , title , type ){
+
+    var finalShader = shaderText;
+    
+    var readyToLoad = true;
+
+
+    var array = finalShader.split( "$" );
+
+    for( var i = 1; i < array.length; i++ ){
+
+      var chunkName = array[i].split("\n")[0];
+
+      if( this.shaderChunks[chunkName] ){
+
+        var tmpShader = finalShader.split( "$" + chunkName );
+
+        finalShader = tmpShader.join( this.shaderChunks[chunkName] );
+
+      }else{
+
+        readyToLoad = false;
+        this.loadShaderChunk( chunkName );
+
+      }
+
+    }
+
+    if( readyToLoad ){    
+     
+      if( type == 'vertex' ){
+        this.vertexShaders[ title ] = finalShader;
+      }else if( type == 'fragment' ){
+        this.fragmentShaders[ title ] = finalShader;
+      }else if( type == 'simulation' ){
+        this.simulationShaders[ title ] = finalShader;
+      }
+
+      this._endLoad( finalShader , title , type );
+
+    }else{
+
+      var self = this;
+      setTimeout( function(){
+        self.onShaderLoaded( finalShader , title , type )
+      }, 300 );
+
+    }
+
+  }
+
+
+  // might add something later...
+  ShaderLoader.prototype._beginLoad = function(  shader , title , type  ){
+    this.beginLoad( shader , title , type  );
+  }
+  
+  ShaderLoader.prototype._endLoad = function( shaderText , title , type ){
+    this.shadersLoaded ++;
+    this.endLoad( shaderText , title , type  );
+  }
+
+  ShaderLoader.prototype.endLoad = function(){}
+  ShaderLoader.prototype.beginLoad = function(){}
